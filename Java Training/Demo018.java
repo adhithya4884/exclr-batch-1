ea48ddec792ff04b8e4c37627bed290c4553b684
adhithya4884 <151131142+adhithya4884@users.noreply.github.com>
2025-01-23 14:24:00 +0530

    Add files via upload

---
+++ Java Training/Demo018.java
@@ -0,0 +1,23 @@
+/* Wap convert minutes into the number of years, days.
+Examples:
+ Input : minutes = 3000010 
+ Output : Given minutes has 5 years 258 days 
+ */
+class MinutesToYears {
+    void minToYrs() {
+        int minutes = 3000010;
+        int minutesInYear = 365 * 24 * 60;
+        int years = minutes / minutesInYear;
+        int remainingMinutes = minutes % minutesInYear;
+        int days = remainingMinutes / (24 * 60);
+        
+        System.out.println("Given minutes has " + years + " years " + days + " days");
+    }
+}       
+
+public class Demo018 {
+    public static void main(String[] args) {
+        MinutesToYears obj = new MinutesToYears();
+        obj.minToYrs();
+    }
+}
