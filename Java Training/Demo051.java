5f8b8af0800f3d6dfcced17178ab4b2852c1fa20
adhithya4884 <151131142+adhithya4884@users.noreply.github.com>
2025-02-04 15:07:25 +0530

    Number is Palindrome or Not.

---
+++ Java Training/Demo051.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Demo051 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int originalNumber = scanner.nextInt();
+        int reversedNumber = 0;
+        int number = originalNumber;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+
+        if (originalNumber == reversedNumber) {
+            System.out.println(originalNumber + " is a palindrome.");
+        } else {
+            System.out.println(originalNumber + " is not a palindrome.");
+        }
+    }
+}
