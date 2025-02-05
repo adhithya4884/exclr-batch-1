db69837d7b959b8ab0bae4a07598f35ca5405222
adhithya4884 <151131142+adhithya4884@users.noreply.github.com>
2025-02-05 11:24:53 +0530

    1st, 2nd and Last Element of an Array

---
+++ Java Training/Demo062.java
@@ -0,0 +1,8 @@
+public class Demo062 {
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30, 40, 50}; // Example array
+        System.out.println("1st element: " + array[0]);
+        System.out.println("2nd element: " + array[1]);
+        System.out.println("Last element: " + array[array.length - 1]);
+    }
+}
