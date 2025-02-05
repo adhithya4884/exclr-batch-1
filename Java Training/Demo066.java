1b05c7969eef2054b33c47f9b23d67095a8a64e9
adhithya4884 <151131142+adhithya4884@users.noreply.github.com>
2025-02-05 11:31:26 +0530

    Print Index Values of an Array

---
+++ Java Training/Demo066.java
@@ -0,0 +1,9 @@
+public class Demo066 {
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30, 40, 50}; // Example array
+        System.out.print("Index values of the array: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("Index " + i + ": " + array[i] + " ");
+        }
+    }
+}
