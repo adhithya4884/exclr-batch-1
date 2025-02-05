0c3f502c49c67d3d2dd76ca8a99c90446c936315
adhithya4884 <151131142+adhithya4884@users.noreply.github.com>
2025-02-05 11:25:59 +0530

    1st and Last Element of an Array

---
+++ Java Training/Demo063.java
@@ -0,0 +1,7 @@
+public class Demo063 {
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30, 40, 50}; // Example array
+        int sum = array[0] + array[array.length - 1]; // Add first and last elements
+        System.out.println("The sum of the first and last elements is: " + sum);
+    }
+}
